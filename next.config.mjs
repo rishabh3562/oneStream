7971b6460afc77a1f6efb1efc3e9177974441c7a
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2024-04-17 04:52:08 +0530

    main stable

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+   
+    images: {
+      domains: ['www.gravatar.com'],
+    },
+  
+};
 
 export default nextConfig;
